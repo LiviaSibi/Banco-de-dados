9c5c74d216a4e9c46dbdf729d3936b7eec17501e
LiviaSibi <livia.sibinelli@gmail.com>
2020-02-07 10:05:10 -0300

    atualização 07/02

--- Gufi/Gufi-03-DQL.sql
+++ Gufi/Gufi-03-DQL.sql
@@ -1,30 +1,32 @@
 --SELECT DOS USUARIOS CADASTRADOS
-SELECT ID_Usuario, NomeUsuario, Email, Senha, DataCadastro, Genero, TipoUsuario.TipoUsuario
+SELECT NomeUsuario, Email, DataCadastro, Genero, TipoUsuario.TipoUsuario
 FROM  Usuarios
-INNER JOIN TipoUsuario ON Usuarios.ID_TipoUsuario =  TipoUsuario.ID_TipoUsuario;
+INNER JOIN TipoUsuario ON Usuarios.ID_TipoUsuario = TipoUsuario.ID_TipoUsuario;
 
 --SELECT DAS INSTITUIÇÕES CADASTRADAS
-SELECT * FROM Instituicao;
+SELECT CNPJ, NomeFantasia, Endereco FROM Instituicao;
 
 --SELECT DOS TIPOS DE EVENTO
-SELECT * FROM TipoEvento;
+SELECT TipoEvento.TipoEvento FROM TipoEvento;
 
 --SELECT DOS EVENTOS
-SELECT ID_Evento, NomeEvento, DataEvento, Descricao, AcessoLivre, Instituicao.NomeFantasia, TipoEvento.TipoEvento
+SELECT NomeEvento, DataEvento, Descricao, AcessoLivre, Instituicao.NomeFantasia, TipoEvento.TipoEvento
 FROM  Eventos
-INNER JOIN Instituicao ON Eventos.ID_Instituicao =  Instituicao.ID_Instituicao
-INNER JOIN TipoEvento ON Eventos.ID_TipoEvento =  TipoEvento.ID_TipoEvento;
+INNER JOIN Instituicao ON Eventos.ID_Instituicao = Instituicao.ID_Instituicao
+INNER JOIN TipoEvento ON Eventos.ID_TipoEvento = TipoEvento.ID_TipoEvento;
 
 --SELECT DOS EVENTOS PÚBLICOS
-SELECT ID_Evento, NomeEvento, DataEvento, Descricao, AcessoLivre, Instituicao.NomeFantasia, TipoEvento.TipoEvento
+SELECT NomeEvento, DataEvento, Descricao, AcessoLivre, Instituicao.NomeFantasia, TipoEvento.TipoEvento
 FROM  Eventos
-INNER JOIN Instituicao ON Eventos.ID_Instituicao =  Instituicao.ID_Instituicao
-INNER JOIN TipoEvento ON Eventos.ID_TipoEvento =  TipoEvento.ID_TipoEvento
+INNER JOIN Instituicao ON Eventos.ID_Instituicao = Instituicao.ID_Instituicao
+INNER JOIN TipoEvento ON Eventos.ID_TipoEvento = TipoEvento.ID_TipoEvento
 WHERE AcessoLivre = 1;
 
 --SELECT DOS EVENTOS QUE UM DETERMINADO USUÁRIO PARTICIPA
-SELECT ID_Presenca, Usuarios.NomeUsuario, Eventos.NomeEvento, Situacao
+SELECT Eventos.NomeEvento, TipoEvento.TipoEvento, Eventos.DataEvento, Eventos.AcessoLivre, Eventos.Descricao, Usuarios.NomeUsuario, Instituicao.NomeFantasia
 FROM  Presencas
-INNER JOIN Usuarios ON Presencas.ID_Usuario =  Usuarios.ID_Usuario
-INNER JOIN Eventos ON Presencas.ID_Evento =  Eventos.ID_Evento
+INNER JOIN Usuarios ON Presencas.ID_Usuario = Usuarios.ID_Usuario
+INNER JOIN Eventos ON Presencas.ID_Evento = Eventos.ID_Evento
+INNER JOIN TipoEvento ON Eventos.ID_TipoEvento = TipoEvento.ID_TipoEvento
+INNER JOIN Instituicao ON Eventos.ID_Instituicao = Instituicao.ID_Instituicao
 WHERE Presencas.ID_Usuario = 3;
