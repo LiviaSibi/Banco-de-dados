6cb16a999c91e1b814b70937342f27739aeb7b41
LiviaSibi <livia.sibinelli@gmail.com>
2020-02-07 11:19:43 -0300

    atualização 07/02

--- Gufi/Gufi-03-DQL.sql
+++ Gufi/Gufi-03-DQL.sql
@@ -23,10 +23,10 @@ INNER JOIN TipoEvento ON Eventos.ID_TipoEvento = TipoEvento.ID_TipoEvento
 WHERE AcessoLivre = 1;
 
 --SELECT DOS EVENTOS QUE UM DETERMINADO USUÁRIO PARTICIPA
-SELECT Eventos.NomeEvento, TipoEvento.TipoEvento, Eventos.DataEvento, Eventos.AcessoLivre, Eventos.Descricao, Usuarios.NomeUsuario, Instituicao.NomeFantasia
+SELECT Eventos.NomeEvento, TipoEvento.TipoEvento, Eventos.DataEvento, CAST(CASE WHEN AcessoLivre=1 THEN 'Publico' ELSE 'Privado' END AS VARCHAR(255)) AS AcessoLivre, Eventos.Descricao, Usuarios.NomeUsuario, Instituicao.NomeFantasia, Situacao
 FROM  Presencas
 INNER JOIN Usuarios ON Presencas.ID_Usuario = Usuarios.ID_Usuario
 INNER JOIN Eventos ON Presencas.ID_Evento = Eventos.ID_Evento
 INNER JOIN TipoEvento ON Eventos.ID_TipoEvento = TipoEvento.ID_TipoEvento
 INNER JOIN Instituicao ON Eventos.ID_Instituicao = Instituicao.ID_Instituicao
-WHERE Presencas.ID_Usuario = 3;
+WHERE Presencas.ID_Usuario = 2 AND Presencas.Situacao LIKE 'Confirmada%';
