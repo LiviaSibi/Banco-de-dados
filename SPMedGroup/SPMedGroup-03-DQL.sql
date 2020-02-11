bdcd689d5fd4549d6fb8206b79d791535d749813
LiviaSibi <livia.sibinelli@gmail.com>
2020-02-11 14:25:03 -0300

    atualização 11/02

--- SPMedGroup/SPMedGroup-03-DQL.sql
+++ SPMedGroup/SPMedGroup-03-DQL.sql
@@ -1,4 +1,5 @@
-SELECT Titulo as Especializacao FROM Especializacao;
+--quantidade de usuários
+SELECT COUNT FROM Usuarios;
 
 SELECT CRM, Nome, Usuarios.Email, Especializacao.Titulo as Especialidade, Clinica.NomeFantasia, Clinica.CNPJ, Clinica.RazaoSocial, Enderecos.Rua, Enderecos.Numero, Enderecos.CEP
 FROM Medicos
@@ -12,8 +13,12 @@ FROM Prontuarios
 INNER JOIN Usuarios ON Prontuarios.ID_Usuario = Usuarios.ID_Usuario
 INNER JOIN Enderecos ON Prontuarios.ID_Endereco = Enderecos.ID_Endereco;
 
-SELECT Prontuarios.Nome as Prontuario, Medicos.Nome as Medico, DataConsulta, Situacao.Titulo as Situacao
+--Select documento (**ARRUMAR DATA CONSULTA**)
+SELECT Prontuarios.Nome as Prontuario, Prontuario.RG, Medicos.Nome as Medico, Medico.Especializacao as Especializacao, Clinica.NomeFantasia, Clinica.RazaoSocial,
+DataConsulta, Situacao.Titulo as Situacao
 FROM Consulta
 INNER JOIN Prontuarios ON Consulta.ID_Prontuario = Prontuarios.ID_Prontuario
 INNER JOIN Medicos ON Consulta.ID_Medico = Medicos.ID_Medico
+INNER JOIN Especializacao ON Medicos.ID_Especializacao = Especializacao.ID_Especializacao
+INNER JOIN Clinica ON Medicos.ID_Clinica = Clinica.ID_Clinica
 INNER JOIN Situacao ON Consulta.ID_Situacao = Situacao.ID_Situacao;
